1cf017b2d8127d818d952923dd2f8f862a4f9d28
tayutaedomo <tayutaedomo@gmail.com>
2023-07-08 16:07:53 +0900

    TipBBS を BBS を使ってリファクタリング

--- test/TipBBS.test.ts
+++ test/TipBBS.test.ts
@@ -12,55 +12,6 @@ describe("TipBBS", function () {
     return { bbs, owner, account1, account2 };
   }
 
-  describe("post", function () {
-    it("投稿することができる", async function () {
-      const { bbs, account1 } = await loadFixture(deployFixture);
-      const postId = 1;
-      const text = "First Post";
-
-      await bbs.connect(account1).post(text);
-
-      const events = await bbs.queryFilter(bbs.filters.Posted(postId));
-
-      expect(await bbs.latestPostId()).to.equal(postId);
-      expect(events[0].args.postId).to.equal(postId);
-      expect(events[0].args.user).to.equal(account1.address);
-      expect(events[0].args.text).to.equal(text);
-    });
-  });
-
-  describe("reply", function () {
-    it("返信することができる", async function () {
-      const { bbs, account1, account2 } = await loadFixture(deployFixture);
-      const parentPostId = 1;
-      const postId1 = 2;
-      const postId2 = 3;
-      const text1 = "Reply Post #1";
-      const text2 = "Reply Post #2";
-
-      await bbs.connect(account1).post("First Post");
-      await bbs.connect(account2).reply(parentPostId, text1);
-
-      await expect(bbs.connect(account2).reply(postId1 + 1, text1)).to.be.revertedWith("Parent post does not exist");
-
-      await bbs.connect(account1).reply(parentPostId, text2); // 自身の投稿に返信することが可能
-
-      const events = await bbs.queryFilter(bbs.filters.Replied(undefined, undefined, parentPostId));
-
-      expect(await bbs.latestPostId()).to.equal(postId2);
-
-      expect(events[0].args.postId).to.equal(postId1);
-      expect(events[0].args.user).to.equal(account2.address);
-      expect(events[0].args.parentPostId).to.equal(parentPostId);
-      expect(events[0].args.text).to.equal(text1);
-
-      expect(events[1].args.postId).to.equal(postId2);
-      expect(events[1].args.user).to.equal(account1.address);
-      expect(events[1].args.parentPostId).to.equal(parentPostId);
-      expect(events[1].args.text).to.equal(text2);
-    });
-  });
-
   describe("like", function () {
     it("like することができる", async function () {
       const { bbs, account1, account2 } = await loadFixture(deployFixture);
